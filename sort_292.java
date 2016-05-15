045024d9bb381663d6531051ffa82bd444cfee9c
xufly <523230758@qq.com>
2016-05-15 13:25:15 +0800

    292

---
+++ sort_292.java
@@ -0,0 +1,7 @@
+package leetcode;
+// you can win by taking just enough to leave four stones for your opponent so that they lose
+public class sort_292 {
+	    public boolean canWinNim(int n) {
+	        return n%4!=0;
+	    }
+}
