9212fd46df82c7301e489975024906f360c3a445
xufly <523230758@qq.com>
2016-06-26 22:41:42 +0800

    3

---
+++ sort_3.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class sort_3 {
+	public static  int lengthOfLongestSubstring(String s) {
+		int i=0,j=0,max=0;
+		Set<Character> set=new HashSet<>();
+		while(j<s.length()){
+			if(!set.contains(s.charAt(j))){
+				set.add(s.charAt(j));
+				j++;
+				max=Math.max(max,set.size());
+			}
+			else{
+				set.remove(s.charAt(i));
+				i++;
+			}
+		}
+		return max;
+		
+	}
+	public static void main(String[] args) {
+		String s="pwwkew";
+		System.out.println(lengthOfLongestSubstring(s));
+	} 
+
+}
